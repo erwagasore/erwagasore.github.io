83a20fe6700114aa2ecebe28c3827a938c973fd7
Eugene Rwagasore <rwagasore@gmail.com>
2021-08-06 01:28:59 +0200

    Clear slate for a presonal blog

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+export default {
+  projectRoot: '.',
+  pages: './src/pages',
+  dist: './dist',
+  public: './public',
+  buildOptions: {
+    site: 'http://rwgsr.com',
+    sitemap: true,
+  },
+  devOptions: {
+    port: 9000
+  },
+};
